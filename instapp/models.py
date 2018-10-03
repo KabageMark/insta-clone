72c4a7f364cb5e545e1eb1b95dc4a91513c4b35b
kabagemark <kabagemark@gmail.com>
2018-10-03 17:05:42 +0300

    Image model created

--- instapp/models.py
+++ instapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to='images-folder')
+    image_name = models.CharField(max_length =30)
+    image_caption = models.CharField(max_length =30)
+    image_profile = ForeignKey(Profile)
+    image_likes = models.PositiveIntegerField(max_length =30)
+    image_comments = models.CharField(max_length =30)
