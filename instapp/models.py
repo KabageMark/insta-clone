5fd13ce6c562f785682bdcb962ebac20401617df
kabagemark <kabagemark@gmail.com>
2018-10-03 17:10:39 +0300

    profile class added

--- instapp/models.py
+++ instapp/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to='profile-pics')
+    bio = models.TextField()
+
+    
 class Image(models.Model):
-    image = models.ImageField(upload_to='images-folder')
+    image = models.ImageField(upload_to='images-uploaded')
     image_name = models.CharField(max_length =30)
     image_caption = models.CharField(max_length =30)
     image_profile = ForeignKey(Profile)
