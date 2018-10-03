731e0938b4bf7e086da439f4ab6ac3dcec218b7b
kabagemark <kabagemark@gmail.com>
2018-10-03 17:25:19 +0300

    update,save,delete methods added to Image class

--- instapp/models.py
+++ instapp/models.py
@@ -5,11 +5,23 @@ class Profile(models.Model):
     profile_photo = models.ImageField(upload_to='profile-pics')
     bio = models.TextField()
 
-    
+
 class Image(models.Model):
     image = models.ImageField(upload_to='images-uploaded')
     image_name = models.CharField(max_length =30)
     image_caption = models.CharField(max_length =30)
     image_profile = ForeignKey(Profile)
-    image_likes = models.PositiveIntegerField(max_length =30)
-    image_comments = models.CharField(max_length =30)
+    image_likes = models.PositiveIntegerField()
+    image_comments = models.TextField()
+    
+    @classmethod
+    def update_caption(cls,current_value,new_value):
+        fetched_object = Image.objects.filter(name=current_value).update(name=new_value)
+        return fetched_object
+
+
+    def save_image(self):
+        return self.save()
+        
+    def delete_image(self):
+        return self.delete()
