ad2d7cb37c8d06d95190356361b040b3c89be164
kabagemark <kabagemark@gmail.com>
2018-10-05 16:55:30 +0300

    login required decorator added to newpost views

--- instapp/views.py
+++ instapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
 from .forms import NewPostForm
 
 # Create your views here.
@@ -8,7 +9,7 @@ def Posts(request):
 
 
 #.....
-# @login_required(login_url='/accounts/login/')
+@login_required(login_url='/accounts/login/')
 def NewPost(request):
     current_user = request.user
     if request.method == 'POST':
